c753303784d545c84e1dc1c31c61502d6b8292ff
Hecto <Hecto@Hector>
2024-06-15 18:08:17 -0600

    0.1 Ventanas

---
+++ src/main/java/com/gfa/ejerciciopracticogfa/EjercicioPracticoGFA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gfa.ejerciciopracticogfa;
+
+/**
+ *
+ * @author Hecto
+ */
+public class EjercicioPracticoGFA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
